de001ff04cde2a33bfc0b2bcbfe2c6dc2623aac3
koryhurd <82877668+koryhurd@users.noreply.github.com>
2021-04-23 12:15:52 -0400

    Update interpreter_bigstep.ml

--- interpreter_bigstep/syntax/interpreter_bigstep.ml
+++ interpreter_bigstep/syntax/interpreter_bigstep.ml
@@ -7,7 +7,7 @@ type exp =
   | Plus of exp * exp
   | Mult of exp * exp
 
-
+(*Sytax*)
   let rec string_of_exp (e : exp) =
     match e with
     | IsZero (expression) ->
@@ -26,20 +26,3 @@ type exp =
 
   let print_expr e =
     print_endline (string_of_exp e);;
-
-  print_expr (Num 3);;
-  print_expr (True);;
-  print_expr (False);;
-  print_expr (Plus (Num 3, Num 2));;
-  print_expr (Mult (Num 3, Num 2));;
-  print_expr (Plus (Num 3, Plus (Num 3, Mult (Num 2, Plus (Num 3, Num 2)))));;
-  print_expr (If (True, Num 3, Num 5));;
-  print_expr (If (False, Plus (Num 3, Num 2), Plus (Num 5, Num 1)));;
-  print_expr (If (Plus (False, True), Plus (Num 3, False), Mult (Num 3, Num 1)));;
-  print_expr (If (IsZero (Num 1), Plus (Num 3, Num 2), Plus (Num 5, Num 1)));;
-  print_expr (IsZero(Mult (Num 3, Num 5)));;
-  print_expr (IsZero( If( IsZero(Num 1), Plus (Num 3, Num 5), Plus (Num 5, Num 1))));;
-  print_expr (Plus (Num 3, If (IsZero (Num 1), Plus (Num 3, Num 5), Plus (Num 5, Num 1))));;
-  print_expr (Plus (Num 3, Mult (If (IsZero(Num 1), Plus (Num 3, Num 5), Plus (Num 5, Num 1)), IsZero(True))));;
-  print_expr (If (If (True, True, False), Plus (Num 3, Num 2), Plus (Num 5, Num 1)));;
-  print_expr (If (True, If (IsZero( Mult (Num 3, Num 5)), Plus (Num 3, Num 2), Plus (Num 5, Num 1)), If (True, Mult (Num 3, Num 2), Mult (Num 2, Plus (Num 3, Num 2)))));;
